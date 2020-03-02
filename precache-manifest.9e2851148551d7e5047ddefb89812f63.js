e5028b60ff238c522b0d48dac384c236febff70f
LisaVerhoeven <lisaverhoeven2001@hotmail.com>
2020-03-02 19:03:04 +0100

    Updates

---
+++ precache-manifest.9e2851148551d7e5047ddefb89812f63.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ac96af26c91e91c68f1ce5ed6b22925",
+    "url": "/kastanjeboom/index.html"
+  },
+  {
+    "revision": "c0a268853311a1fc7753",
+    "url": "/kastanjeboom/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "842e9aeaf58c4ecde33f",
+    "url": "/kastanjeboom/static/css/main.2a2117bf.chunk.css"
+  },
+  {
+    "revision": "c0a268853311a1fc7753",
+    "url": "/kastanjeboom/static/js/2.a3718dbc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/kastanjeboom/static/js/2.a3718dbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "842e9aeaf58c4ecde33f",
+    "url": "/kastanjeboom/static/js/main.daf5dc80.chunk.js"
+  },
+  {
+    "revision": "b183a28ffcc6614fa3dc",
+    "url": "/kastanjeboom/static/js/runtime-main.c00b1c45.js"
+  },
+  {
+    "revision": "25cdcfbb2652ba07a8920a5d7d57eb6a",
+    "url": "/kastanjeboom/static/media/pictureoutside.25cdcfbb.jpg"
+  },
+  {
+    "revision": "e478ae2ad0a78655c192e682c99ac5c5",
+    "url": "/kastanjeboom/static/media/woodbackground.e478ae2a.jpg"
+  }
+]);
